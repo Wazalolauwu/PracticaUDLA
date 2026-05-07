dd0496389ff16349e7cb3629ead194327fb6a7bb
Wazalolauwu <vargaslola06@gmail.com>
2026-05-07 12:18:22 -0500

    Creacion de objetos tipo LuzInteligente

--- src/primerejercicio/interfaz/Main.java
+++ src/primerejercicio/interfaz/Main.java
@@ -1,8 +1,11 @@
 package primerejercicio.interfaz;
 
+import primerejercicio.negocio.LuzInteligente;
+
 public class Main{
     public static void main() {
-
+        LuzInteligente l1 = new LuzInteligente("A113");
+        LuzInteligente l2 = new LuzInteligente("A001");
 
     }
 
--- src/primerejercicio/negocio/LuzInteligente.java
+++ src/primerejercicio/negocio/LuzInteligente.java
@@ -2,7 +2,35 @@ package primerejercicio.negocio;
 
 public class LuzInteligente {
     private String idLuz;
-    private String nivelBrillo;
+    private int nivelBrillo;
+    private boolean encendido;
+
+    public LuzInteligente(String idLuz) {
+        this.idLuz = idLuz;
+    }
+
+    public void encender() {
+        this.encendido = true;
+    }
+
+    public void apagar() {
+        this.encendido = false;
+    }
+
+    public void ajustarbrillo(int nuevoNivel) {
+        if (encendido == true) {
+            this.nivelBrillo = nuevoNivel;
+        }
+    }
+    public void mostrarEstado(){
+        if (encendido==true){
+            System.out.println("La luz esta encendida");
+            System.out.println("El nivel de brillo es"+nivelBrillo);
+        }else{
+            System.out.println("La luz esta apagada");
+        }
+    }
+
 
 
 }
